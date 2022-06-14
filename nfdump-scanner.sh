ab8bc9b6a37dbea126fb521818580926720ed03b
bdorr1105 <12386911+bdorr1105@users.noreply.github.com>
2022-06-13 21:46:38 -0400

    Update nfdump-scanner.sh

--- nfdump-scanner.sh
+++ nfdump-scanner.sh
@@ -3,7 +3,18 @@
 #########Variables Section##########
 RED='\033[0;31m'
 NOCOLOR='\033[0m'
+firewall_nat='x.x.x.x' # enter your firewall or router LAN gateway
+firewall_nat='x.x.x.x'  #enter your public IP, you can google what is my ip to get this
+scan1='Top 10 IP Address and Destination Ports ordered bytes by ip'
+scan2='Top 10 IP address with a destination port of 80 and 443'
+scan3='Top 10 source IP by protocol'
+scan4='Top 10 Flows by source and destination'
+command1='nfdump -R /home/netflow/logs/netflow/pfsense/ -s ip/bytes -s dstport:p/bytes -n 10'
+command2="nfdump -R /home/netflow/logs/netflow/pfsense/ -c 10 'proto tcp and (dst port 80 or dst port 443) and not src host $firewall_lan and not src host $firewall_nat'"
+command3="nfdump -R /home/netflow/logs/netflow/pfsense/ -O bytes -A srcip,proto,dstport -n 10 -o 'fmt:%sa -> %pr %dp %byt %fl'"
+command4='nfdump -R /home/netflow/logs/netflow/pfsense/ -a -O tstart -n 10'
 ##################################
+clear # clear the screen of all garbage for a clean experience
 echo "############################"
 echo "#                          #"
 echo "#     nfdump scanner       #"
@@ -15,65 +26,82 @@ echo "Choose which nfdump scan you want to run below"
 ###### Enter selection appears below the choices #######
 echo " "
 PS3='Enter Selection: '
-scans=("Top 10 IP Address and Destination Ports ordered bytes by ip" "Top 10 source IP addresses ordered bytes by ip" "Top 10 source IP by protocol" "Top 10 Flows by source and destination" "Quit")
+scans=("$scan1" "$scan2" "$scan3" "$scan4" "Quit")
 select hunt in "${scans[@]}"; do
     case $hunt in
-        "Top 10 IP Address and Destination Ports ordered bytes by ip")
+        $scan1)
             clear
-            echo -e "${RED} The command ran was --> nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -s ip/bytes -n 10${NOCOLOR}"
-            nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -s ip/bytes -s dstport:p/bytes -n 10
-	    # optionally call a function or run some code here
-            echo " "
-            echo "1. Top 10 IP Address and Destination Ports ordered bytes by ip "
-            echo "2. Top 10 source IP addresses ordered bytes by ip"
-            echo "3. Top 10 source IP by protocol"
-            echo "4. Top 10 Flows by source and destination"
-            echo "5. Quit"
-            echo " "
-            ;;
-        "Top 10 source IP addresses ordered bytes by ip")
-            clear
-            echo -e "${RED}The command ran was --> nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -s srcip/bytes${NOCOLOR}"
-	        nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -s srcip/bytes
-            echo " "
-            echo "1. Top 10 IP Address and Destination Ports ordered bytes by ip "
-            echo "2. Top 10 source IP addresses ordered bytes by ip"
-            echo "3. Top 10 source IP by protocol"
-            echo "4. Top 10 Flows by source and destination"
-            echo "5. Quit"
-            echo " "
-            ;;
-        "Top 10 source IP by protocol")
-            clear
-            echo -e "${RED}The command ran was -- >  nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -O bytes -A srcip,proto,dstport -n 10 -o 'fmt:%sa -> %pr %dp %byt %fl'${NOCOLOR}"
-            nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -O bytes -A srcip,proto,dstport -n 10 -o 'fmt:%sa -> %pr %dp %byt %fl'
-	    # optionally call a function or run some code here
-            echo " "
-            echo "1. Top 10 IP Address and Destination Ports ordered bytes by ip "
-            echo "2. Top 10 source IP addresses ordered bytes by ip"
-            echo "3. Top 10 source IP by protocol"
-            echo "4. Top 10 Flows by source and destination"
-            echo "5. Quit"
-            echo " "
-	    ;;
-        "Top 10 Flows by source and destination")
-            clear
-            echo -e "${RED}The command ran was -- >  nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -a -O tstart -n 10${NOCOLOR}"
-            nfdump -R /root/path/of/your/ncapd.xxxx/logs/ -a -O tstart -n 10
+            echo -e "${RED}The command ran was --> $command1${NOCOLOR}"
+            #$command1
             # optionally call a function or run some code here
+            mycommand1() {
+                      ${command1}
+            }
+            mycommand1
             echo " "
-            echo "1. Top 10 IP Address and Destination Ports ordered bytes by ip "
-            echo "2. Top 10 source IP addresses ordered bytes by ip"
-            echo "3. Top 10 source IP by protocol"
-            echo "4. Top 10 Flows by source and destination"
+            echo "1. $scan1"
+            echo "2. $scan2"
+            echo "3. $scan3"
+            echo "4. $scan4"
+            echo "5. Quit"
+            echo " "
+            ;;
+        $scan2)
+            clear
+            echo -e "${RED}The command ran was --> $command2${NOCOLOR}"
+            #$command2
+            # Had to use a function with eval to get this to Run the Scan. The difference between this command and commands (1,4) is there are paratnheseis and single quotes
+            mycommand2() {
+                       eval ${command2}
+            }
+            mycommand2
+            echo " "
+            echo "1. $scan1"
+            echo "2. $scan2"
+            echo "3. $scan3"
+            echo "4. $scan4"
+            echo "5. Quit"
+            echo " "
+            ;;
+        $scan3)
+            clear
+            echo -e "${RED}The command ran was -- >  $command3${NOCOLOR}"
+            #$command3
+            # optionally call a function or run some code here
+            mycommand3() {
+                       eval ${command3}
+            }
+            mycommand3
+            echo " "
+            echo "1. $scan1"
+            echo "2. $scan2"
+            echo "3. $scan3"
+            echo "4. $scan4"
+            echo "5. Quit"
+            echo " "
+            ;;
+        $scan4)
+            clear
+            echo -e "${RED}The command ran was -- >  $command4${NOCOLOR}"
+            #$command4
+            # optionally call a function or run some code here
+            mycommand4() {
+                      ${command4}
+            }
+            mycommand4
+            echo " "
+            echo "1. $scan1"
+            echo "2. $scan2"
+            echo "3. $scan3"
+            echo "4. $scan4"
             echo "5. Quit"
             echo " "
             #break
             ;;
-	"Quit")
-	    echo "User requested exit"
-	    exit
-	    ;;
+        "Quit")
+            echo "User requested exit"
+            exit
+            ;;
         *) echo "invalid option $REPLY";;
     esac
 done
